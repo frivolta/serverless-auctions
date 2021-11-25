2467c3b5ad5fd920a3b62227ed3a80b13cad0c34
frivolta <rivoltafilippo@gmail.com>
2021-11-25 12:20:54 +0100

    Adding validation

--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -1,12 +1,15 @@
 import AWS from "aws-sdk";
 import createError from "http-errors";
+import validator from "@middy/validator";
 import commonMiddleware from "../lib/commonMiddleware";
+import getAuctionsSchema from "../lib/schemas/getAuctionsSchema";
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function getAuctions(event, context) {
   const { status } = event.queryStringParameters;
   let auctions;
+
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
     IndexName: "statusAndEndDate",
@@ -18,12 +21,14 @@ async function getAuctions(event, context) {
       "#status": "status",
     },
   };
+
   try {
-    const result = await dynamoDb.query(params).promise();
+    const result = await dynamodb.query(params).promise();
+
     auctions = result.Items;
-  } catch (err) {
-    console.error(err);
-    throw new createError.InternalServerError(err.message);
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
   }
 
   return {
@@ -32,4 +37,12 @@ async function getAuctions(event, context) {
   };
 }
 
-export const handler = commonMiddleware(getAuctions);
+export const handler = commonMiddleware(getAuctions).use(
+  validator({
+    inputSchema: getAuctionsSchema,
+    ajvOptions: {
+      useDefaults: true,
+      strict: false,
+    },
+  })
+);
